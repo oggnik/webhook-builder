ea6a9bae01ff3e6ab763a7673f3664d89eaac18f
Nicki Ogg <oggnicki@yahoo.com>
2015-03-05 14:21:29 -0500

    run command

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+var spawn   = require('child_process').spawn;
 var express = require('express');
 
 var app = express();
+
+// For easy editing
+var port = 3205;
+var script = "sendCommandTMUX"
 
 
 /*
@@ -8,11 +13,38 @@ var app = express();
 	This is used to submit youtube links
 */
 app.post('/webhook', function(req, res) {
+	console.log("Request received");
 	console.log(req.body);
+	var command = spawn(__dirname + '/' + script);
+	var output = [];
+	var errorOut = [];
+
+	command.stdout.on('data', function(chunk) {
+		output.push(chunk);	
+	});
+	command.stderr.on('data', function(chunk) {
+		errorOut.push(chunk);	
+	});
+
+	command.on('close', function(code) {
+		if (code === 0) {
+			// We are good
+			console.log("Command executed");
+		} else {
+			// Something went wrong
+			console.log("The script did not return with code 0");
+			console.log("Output");
+			console.log(Buffer.concat(output));
+			console.log("Stderr Output");
+			console.log(Buffer.concat(errorOut));
+		}
+	});
+
+	res.end();
 });
 
 
-var server = app.listen(3205, function() {
+var server = app.listen(port, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
